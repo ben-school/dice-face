ed20145b46931ceec54f7b40baaf1d13efd49b50
ben-school <144243323+ben-school@users.noreply.github.com>
2023-10-04 14:35:21 +0100

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,16 @@
+namespace Dice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ooooooooooo");
+            Console.WriteLine("o         o");
+            Console.WriteLine("o  #   #  o");
+            Console.WriteLine("o    #    o");
+            Console.WriteLine("o  #   #  o");
+            Console.WriteLine("o         o");
+            Console.WriteLine("ooooooooooo");
+        }
+    }
+}
